cf69f0e2247c816bc3048d26ff24dcddfc66aaae
ayat kalybekova <kalybekAyat@outlook.com>
2024-11-13 13:50:36 +0600

    finish

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,7 +1,68 @@
-const Footer = () => {
+import { FC } from "react";
+import scss from "./footer.module.scss";
+import {
+  FaTiktok,
+  FaInstagram,
+  FaYoutube,
+  FaFacebookF,
+  FaTwitter,
+} from "react-icons/fa";
+
+const Footer: FC = () => {
   return (
-    <footer>
-      <div className="container">footer</div>
+    <footer className={scss.Footer}>
+      <div className="container">
+        <div className={scss.newsletter}>
+          <h3>You have the power to define your future.</h3>
+          <form>
+            <input type="email" placeholder="Email Address" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+        <div className={scss.footer_content}>
+          <div className={scss.section}>
+            <h4>Learning Platform</h4>
+            <a href="#">Home</a>
+            <a href="#">Courses</a>
+            <a href="#">Free Workshops</a>
+            <a href="#">Blog</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </div>
+          <div className={scss.section}>
+            <h4>Template</h4>
+            <a href="#">Instructions</a>
+            <a href="#">Style Guide</a>
+            <a href="#">Licenses</a>
+            <a href="#">Changelog</a>
+          </div>
+          <div className={scss.section} style={{ alignItems: "center" }}>
+            <h4>Follow Skillz</h4>
+            <div className={scss.social}>
+              <a href="#">
+                <FaTiktok />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+              <a href="#">
+                <FaYoutube />
+              </a>
+              <a href="#">
+                <FaFacebookF />
+              </a>
+              <a href="#">
+                <FaTwitter />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className={scss.copyright}>
+          © Skillz Template Powered by Webflow
+        </div>
+      </div>
     </footer>
   );
 };
